0b4921800322a1b6b99d87243990bc59a5227b63
Yukio Shimonohara <yshimonohara@cv-lab.com>
2017-05-24 08:29:49 +0900

    プロジェクト ファイルを追加します。

---
+++ WPFSample_PrismUnity/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using WPFSample_PrismUnity.Views;
+using System.Windows;
+
+namespace WPFSample_PrismUnity
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
