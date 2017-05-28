0961fc2b6ef678c1b045759aeb48b4501f99b939
Yukio Shimonohara <yshimonohara@cv-lab.com>
2017-05-28 22:58:14 +0900

    コントロールサンプル画面にボタンコントロール追加

--- WPFSample_PrismUnity/ViewModels/BasicControlViewModel.cs
+++ WPFSample_PrismUnity/ViewModels/BasicControlViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,24 @@ namespace WPFSample_PrismUnity.ViewModels
 {
     public class BasicControlViewModel : BindableBase
     {
+        private Dictionary<string, string> ControlExplanations { get; set; } = new Dictionary<string, string>()
+        {
+            {
+                "Button",
+                "<Button Grid.Column=\"1\" Content=\"Button\" VerticalAlignment=\"Center\" Margin=\"10\" Width=\"100\" HorizontalAlignment=\"Left\"/>"
+            },
+        };
+
+        public ReactiveProperty<string> ButtonControlExplanation { get; set; } = new ReactiveProperty<string>();
+
         public BasicControlViewModel()
         {
+            InitReactiveProperties();
+        }
 
+        private void InitReactiveProperties()
+        {
+            ButtonControlExplanation.Value = ControlExplanations["Button"];
         }
     }
 }
